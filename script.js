9cb72c0f104f4bb8a86b0a4284f8e2ba5900e1e5
Semidime <semidime@protonmail.com>
2024-01-08 14:38:52 +0000

    feat: basic tic tac toe console version
    uses gameboard.gameArray to track the game board.
    
    makeMove function allows players to play via the console (requires index and token value to be submitted to as arguments).
    
    checkWinCondition function evaluates win conditions and logs message to console when win achieved.

---
+++ script.js
@@ -0,0 +1,44 @@
+const gameboard = {
+    gameArray: ["","","","","","","","",""],
+}
+
+console.log(gameboard.gameArray)
+
+// const gameManager {
+    //MANAGE TURN
+    //MAKE MOVE
+    //CHECK WIN/DRAW CONDITION
+    //DECLARE A WINNER
+    //RESET BOARD
+//}
+
+function makeMove(index,xOrO) {
+    if (gameboard.gameArray[index]=="") {
+        gameboard.gameArray.splice(index,1,xOrO);   
+        console.log(gameboard.gameArray);
+    } else if (gameboard.gameArray[index]!="") {
+        console.log("move not available",gameboard.gameArray)
+    }
+
+    checkWinCondition();
+}
+
+function checkWinCondition() {
+    if (gameboard.gameArray[0] === gameboard.gameArray[1] && gameboard.gameArray[0] === gameboard.gameArray[2] && gameboard.gameArray[0]!="") {
+        console.log("WINNER");
+    } else if (gameboard.gameArray[3] === gameboard.gameArray[4] && gameboard.gameArray[3] === gameboard.gameArray[5] && gameboard.gameArray[3]!="") {
+        console.log("WINNER"); 
+    } else if (gameboard.gameArray[6] === gameboard.gameArray[7] && gameboard.gameArray[6] === gameboard.gameArray[8] && gameboard.gameArray[6]!="") {
+        console.log("WINNER");   
+    } else if (gameboard.gameArray[0] === gameboard.gameArray[3] && gameboard.gameArray[0] === gameboard.gameArray[6] && gameboard.gameArray[0]!="") {
+        console.log("WINNER");       
+    } else if (gameboard.gameArray[1] === gameboard.gameArray[4] && gameboard.gameArray[1] === gameboard.gameArray[7] && gameboard.gameArray[1]!="") {
+        console.log("WINNER"); 
+    } else if (gameboard.gameArray[2] === gameboard.gameArray[5] && gameboard.gameArray[2] === gameboard.gameArray[8] && gameboard.gameArray[2]!="") {
+        console.log("WINNER"); 
+    } else if (gameboard.gameArray[0] === gameboard.gameArray[4] && gameboard.gameArray[0] === gameboard.gameArray[8] && gameboard.gameArray[0]!="") {
+        console.log("WINNER"); 
+    } else if (gameboard.gameArray[2] === gameboard.gameArray[4] && gameboard.gameArray[2] === gameboard.gameArray[6] && gameboard.gameArray[2]!="") {
+        console.log("WINNER"); 
+    } else return
+}
