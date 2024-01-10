671470c8cb82ae054d81285f634a7d9560eabc4b
Semidime <semidime@protonmail.com>
2024-01-10 12:30:02 +0000

    feat: added events object to manage checkWinCondition
    - gameboard module publishes updated gameboard on each change
    - checkWinCondition function gameManager module.
    - gameManager will checkWindCondition each time gameboard published an updated board to events module.

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
+//PubSub module
+const events = {
+    events: {},
+    on: function (eventName, fn) {
+        this.events[eventName] = this.events[eventName] || [];
+        this.events[eventName].push(fn);
+    },
+    off: function(eventName, fn) {
+        if (this.events[eventName]) {
+        for (var i = 0; i < this.events[eventName].length; i++) {
+            if (this.events[eventName][i] === fn) {
+            this.events[eventName].splice(i, 1);
+            break;
+            }
+        };
+        }
+    },
+    emit: function (eventName, data) {
+        if (this.events[eventName]) {
+        this.events[eventName].forEach(function(fn) {
+            fn(data);
+        });
+        }
+    }
+    };
+
 const gameboard = (function () {
     const gameArray = ["","","","","","","","",""];
    
     function updateGameArray(index, token) {
         gameArray.splice(index,1,token)
-        _render();        
-        _checkWinCondition();
+        events.emit("gameArrayChanged", gameArray);  
+        _render();              
     }
 
     function _render() {
         console.log(`GAMEBOARD: ${gameArray}`);
         //access DOM module [TO BE ADDED]
-    }
-
-    function _checkWinCondition() {
-        if (gameArray[0] === gameArray[1] && gameArray[0] === gameArray[2] && gameArray[0]!="") {
-            gameManager.announceResult("winner");
-        } else if (gameArray[3] === gameArray[4] && gameArray[3] === gameArray[5] && gameArray[3]!="") {
-            gameManager.announceResult("winner"); 
-        } else if (gameArray[6] === gameArray[7] && gameArray[6] === gameArray[8] && gameArray[6]!="") {
-            gameManager.announceResult("winner");   
-        } else if (gameArray[0] === gameArray[3] && gameArray[0] === gameArray[6] && gameArray[0]!="") {
-            gameManager.announceResult("winner");       
-        } else if (gameArray[1] === gameArray[4] && gameArray[1] === gameArray[7] && gameArray[1]!="") {
-            cgameManager.announceResult("winner"); 
-        } else if (gameArray[2] === gameArray[5] && gameArray[2] === gameArray[8] && gameArray[2]!="") {
-            gameManager.announceResult("winner"); 
-        } else if (gameArray[0] === gameArray[4] && gameArray[0] === gameArray[8] && gameArray[0]!="") {
-            gameManager.announceResult("winner"); 
-        } else if (gameArray[2] === gameArray[4] && gameArray[2] === gameArray[6] && gameArray[2]!="") {
-            gameManager.announceResult("winner"); 
-        } else if (gameArray.includes("")) {
-            return;
-        } else {
-            gameManager.announceResult("tied");
-        }
+        //could be replaced by subscribing DOM module to gameArrayChanged event
     }
 
     return { updateGameArray }
+    // ADD PLAYER NAME DISPLAY
+    // ADD SCOREBOARD 
 })();
 
 const gameManager = (function () {
 
     let currentTurn = "X";
-    const availableMoves = [0,1,2,3,4,5,6,7,8];
+    const availableSquares = [0,1,2,3,4,5,6,7,8];
     let gameState = "ongoing";
-    console.log(`${currentTurn} to play`);
+    events.on("gameArrayChanged",_checkWinCondition);
+    console.log(`${currentTurn} to play.`, `Available squares are ${availableSquares}`); 
     
     function makeMove (moveRef) {
-        if (availableMoves.includes(moveRef)) {
-            availableMoves.splice(availableMoves.indexOf(moveRef),1);
-            console.log(`${currentTurn} played moveRef:${moveRef}`, `Available Moves are ${availableMoves}`);
+        if (availableSquares.includes(moveRef)) {
+            availableSquares.splice(availableSquares.indexOf(moveRef),1);
+            console.log(`${currentTurn} played moveRef:${moveRef}`, `Available squares are ${availableSquares}`);
             gameboard.updateGameArray(moveRef,currentTurn);
 
             if (gameState == "ongoing") {
@@ -58,10 +64,35 @@ const gameManager = (function () {
             }
 
         } else {
-            console.log(`${currentTurn} tried to play moveRef:${moveRef}. That move is not available`, `Available Moves are ${availableMoves}`);
+            console.log(`${currentTurn} tried to play moveRef:${moveRef}. That move is not available.`)
+            console.log(`${currentTurn} to play.`, `Available squares are ${availableSquares}`);
         }
     }
 
+    function _checkWinCondition(currentBoard) {
+        if (currentBoard[0] === currentBoard[1] && currentBoard[0] === currentBoard[2] && currentBoard[0]!="") {
+            _announceResult("winner");
+        } else if (currentBoard[3] === currentBoard[4] && currentBoard[3] === currentBoard[5] && currentBoard[3]!="") {
+            _announceResult("winner"); 
+        } else if (currentBoard[6] === currentBoard[7] && currentBoard[6] === currentBoard[8] && currentBoard[6]!="") {
+            _announceResult("winner");   
+        } else if (currentBoard[0] === currentBoard[3] && currentBoard[0] === currentBoard[6] && currentBoard[0]!="") {
+            _announceResult("winner");       
+        } else if (currentBoard[1] === currentBoard[4] && currentBoard[1] === currentBoard[7] && currentBoard[1]!="") {
+            _announceResult("winner"); 
+        } else if (currentBoard[2] === currentBoard[5] && currentBoard[2] === currentBoard[8] && currentBoard[2]!="") {
+            _announceResult("winner"); 
+        } else if (currentBoard[0] === currentBoard[4] && currentBoard[0] === currentBoard[8] && currentBoard[0]!="") {
+            _announceResult("winner"); 
+        } else if (currentBoard[2] === currentBoard[4] && currentBoard[2] === currentBoard[6] && currentBoard[2]!="") {
+            _announceResult("winner"); 
+        } else if (currentBoard.includes("")) {
+            return;
+        } else {
+            _announceResult("tied");
+        }
+    }
+    
     function _updateCurrentTurn() {
         if (currentTurn === "X") {
             currentTurn = "O";
@@ -70,7 +101,7 @@ const gameManager = (function () {
         }
     }
 
-    function announceResult(outcome) {
+    function _announceResult(outcome) {
         if (outcome == "winner") {
         console.log(`GAME OVER! ${currentTurn} won this round.`);
         } else {
@@ -79,10 +110,12 @@ const gameManager = (function () {
         gameState = "finished";
     }
 
-    return { makeMove , announceResult }
+    return { makeMove }
 })();
 
-    //makeMove too complicated.  Break into separate functions
-    //CONSIDER if cWC should be in gameboard module or gameManager
-    //ADD PLAYER MODULE AND TRACK SCORES
-    //OFFER TO PLAY ANOTHER ROUND AFTER GAME COMPLETED
+    // makeMove too complicated.  Break into separate functions?
+    // ADD PLAYER MODULE - new player module
+    // ADD SCOREBOARD & TRACK SCORES - gameboard module
+    // RESET BOARD TO PLAY ANOTHER ROUND AFTER GAME COMPLETED
+    // ADD DOM MODULE 
+
