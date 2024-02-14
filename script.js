c061a2a217cd92c3b73eec0ed933dab7e4d7f25a
Semidime <semidime@protonmail.com>
2024-02-14 14:18:00 +0000

    feat: applies change to resetGame function.
    - when a new player is added, the first move will always be assigned to the player using the "X" token.

--- script.js
+++ script.js
@@ -326,13 +326,13 @@ const gameManager = (function () {
         if (type === "newPlayer") {
             currentScoreX = 0;
             currentScoreO = 0;
-            currentTurn = firstMove;
+            currentTurn = "X";
             events.emit("scoreChanged", [currentScoreX, currentScoreO] )
         } else if (type === "newRound") {
             currentTurn = firstMove === "X" ? "O" : "X";
-            firstMove = currentTurn;
         }
-        availableSquares = [0,1,2,3,4,5,6,7,8];       
+        availableSquares = [0,1,2,3,4,5,6,7,8];
+        firstMove = currentTurn;
         currentPlayer = currentTurn === "X" ? players.playerX : players.playerO;
         _manageGameBoardListeners("remove");
         _manageGameBoardListeners("add");
